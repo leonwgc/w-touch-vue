541fce0b8e372e1fb19d37f8dea6398e2cb389f2
leonwgc <giantfish@126.com>
2023-02-19 15:19:10 +0800

    docs: online

--- .fatherrc.js
+++
@@ -1,9 +0,0 @@
-export default {
-  entry: 'src/index',
-  cjs: {
-    minify: true,
-  },
-  esm: {
-    minify: true,
-  },
-};
--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,14 +12,24 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  server: {
-    host: true,
-  },
+
   build: {
-    outDir: `docs`,
-    assetsDir: '',
+    outDir: `dist`,
     emptyOutDir: true,
-    manifest: true,
+    lib: {
+      entry: resolve(__dirname, 'src'),
+      name: 'w-touch-vue',
+      fileName: 'w-touch-vue.js',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
   },
-  base: '',
 });
